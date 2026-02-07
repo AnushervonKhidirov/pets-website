37d00664863bcc04129a0948eb3c689af5e4e4c7
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-07 21:28:56 +0500

    header completed

--- app/component/common/header/header.tsx
+++ app/component/common/header/header.tsx
@@ -1,9 +1,9 @@
 import type { NavLinkRenderProps } from 'react-router';
 
+import { useState, useRef, useEffect } from 'react';
 import { NavLink } from 'react-router';
+import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
 import { Route } from '~/constant/route';
-
-import { isMobile } from 'mobile-device-detect';
 
 import Container from '~commons/container/container';
 import Logo from '~commons/logo/logo';
@@ -14,49 +14,74 @@ import classNames from 'classnames';
 import classes from './header.module.css';
 
 const Header = () => {
-    console.log(isMobile);
+    const menuRef = useRef<HTMLDivElement>(null);
+    const menuInnerRef = useRef<HTMLDivElement>(null);
+    const [menuOpened, setMenuOpened] = useState(false);
+
+    function menuHandler() {
+        setMenuOpened(prevState => !prevState);
+    }
+
+    useEffect(() => {
+        if (menuRef.current && menuInnerRef.current) {
+            const menu = menuRef.current;
+            const menuInner = menuInnerRef.current;
+            menu.style.setProperty('--height', `${menuInner.offsetHeight}px`);
+        }
+    }, [menuRef.current, menuInnerRef.current, window.innerWidth]);
 
     return (
         <header className={classes.header}>
             <Container innerClassName={classes.header_inner}>
                 <Logo className={classes.logo} />
 
-                {isMobile ? <MobileNav /> : <DesktopNav />}
+                <div
+                    ref={menuRef}
+                    className={classNames(classes.menu_wrapper, { [classes.opened]: menuOpened })}
+                >
+                    <div ref={menuInnerRef} className={classes.menu_inner}>
+                        <nav className={classes.nav_bar}>
+                            <NavLink
+                                onClick={() => setMenuOpened(false)}
+                                to={Route.Home}
+                                className={getClassName}
+                            >
+                                Главная
+                            </NavLink>
+                            <NavLink
+                                onClick={() => setMenuOpened(false)}
+                                to={Route.Lost}
+                                className={getClassName}
+                            >
+                                Потеряшки
+                            </NavLink>
+                            <NavLink
+                                onClick={() => setMenuOpened(false)}
+                                to={Route.VetClinic}
+                                className={getClassName}
+                            >
+                                Клиники
+                            </NavLink>
+                            <NavLink
+                                onClick={() => setMenuOpened(false)}
+                                to={Route.ContactUs}
+                                className={getClassName}
+                            >
+                                Связаться с нами
+                            </NavLink>
+                        </nav>
+
+                        <Button className={classes.auth_btn} icon={<Paw />}>
+                            Войти
+                        </Button>
+                    </div>
+                </div>
+
+                <button className={classes.menu_icon} onClick={menuHandler}>
+                    {menuOpened ? <CloseOutlined /> : <MenuOutlined />}
+                </button>
             </Container>
         </header>
-    );
-};
-
-const MobileNav = () => {
-    return (
-        <>
-            <div className="menu_icon">menu icon</div>
-        </>
-    );
-};
-
-const DesktopNav = () => {
-    return (
-        <>
-            <nav className={classes.nav_bar}>
-                <NavLink to={Route.Home} className={getClassName}>
-                    Главная
-                </NavLink>
-                <NavLink to={Route.Lost} className={getClassName}>
-                    Потеряшки
-                </NavLink>
-                <NavLink to={Route.VetClinic} className={getClassName}>
-                    Клиники
-                </NavLink>
-                <NavLink to={Route.ContactUs} className={getClassName}>
-                    Связаться с нами
-                </NavLink>
-            </nav>
-
-            <Button className={classes.auth_btn} fullRounded icon={<Paw />}>
-                Войти
-            </Button>
-        </>
     );
 };
 
