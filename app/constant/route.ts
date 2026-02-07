ad278617a7ce6d070c53acfb107c274211a6989b
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-07 20:20:20 +0500

    route paths constant added

---
+++ app/constant/route.ts
@@ -0,0 +1,11 @@
+export enum Route {
+    Home = '/',
+    Lost = '/lost',
+    VetClinic = '/vet-clinic',
+    ContactUs = '/contact-us',
+
+    SignIn = '/sign-in',
+    SignUp = '/sign-up',
+
+    Profile = '/profile',
+}
