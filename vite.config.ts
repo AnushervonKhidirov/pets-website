576d6ad3954c6cc46a7dd1bce22eabe3fd5979f4
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-07 15:31:33 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { reactRouter } from '@react-router/dev/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-    plugins: [reactRouter(), tsconfigPaths()],
+    plugins: [reactRouter(), devtoolsJson(), tsconfigPaths()],
     server: {
         port: 3000,
+        hmr: { overlay: false },
     },
 });
