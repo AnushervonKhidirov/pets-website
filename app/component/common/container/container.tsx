a7c57eef4dc06cf6950401630428e694d55642e4
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-08 17:01:46 +0500

    container updated

--- app/component/common/container/container.tsx
+++ app/component/common/container/container.tsx
@@ -4,11 +4,26 @@ import type { WithClassName } from '~type/common.type';
 import classNames from 'classnames';
 import classes from './container.module.css';
 
-type ContainerProps = PropsWithChildren<WithClassName & { innerClassName?: string }>;
+type ContainerProps = PropsWithChildren<
+    WithClassName & { innerClassName?: string; section?: boolean; colored?: boolean }
+>;
 
-const Container: FC<ContainerProps> = ({ children, className, innerClassName }) => {
+const Container: FC<ContainerProps> = ({
+    section,
+    colored,
+    children,
+    className,
+    innerClassName,
+}) => {
     return (
-        <div className={classNames(classes.container, className)}>
+        <div
+            className={classNames(
+                classes.container,
+                { [classes.colored]: colored },
+                { [classes.section]: section },
+                className,
+            )}
+        >
             <div className={classNames(classes.container_inner, innerClassName)}>{children}</div>
         </div>
     );
