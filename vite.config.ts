ffaedde00834259be72171aec08d77d3e754c1e2
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-08 18:35:53 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [reactRouter(), devtoolsJson(), tsconfigPaths()],
     server: {
         host: '0.0.0.0',
+        allowedHosts: true,
         port: 3000,
         hmr: { overlay: false },
     },
