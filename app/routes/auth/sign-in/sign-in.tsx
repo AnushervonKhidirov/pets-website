f3c9abf2a2eaa12b9a1fef19f2bbf680954262fd
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-09 21:30:54 +0500

    removed

--- app/routes/auth/sign-in/sign-in.tsx
+++
@@ -1,9 +0,0 @@
-import type { Route } from './+types/sign-in';
-
-export function meta({}: Route.MetaArgs) {
-    return [{ title: 'SignIn page' }, { name: 'description', content: 'Welcome to React Router!' }];
-}
-
-export default function SignIn() {
-    return <div>SignIn page</div>;
-}
--- app/routes/auth/sign-up/sign-up.tsx
+++
@@ -1,9 +0,0 @@
-import type { Route } from './+types/sign-up';
-
-export function meta({}: Route.MetaArgs) {
-    return [{ title: 'SignUp page' }, { name: 'description', content: 'Welcome to React Router!' }];
-}
-
-export default function SignUp() {
-    return <div>SignUp page</div>;
-}
