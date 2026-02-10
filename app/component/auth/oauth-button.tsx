d36b96e05b62fe259d9b1d90fb089540a4e6690e
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-10 16:10:39 +0500

    google oauth button added

---
+++ app/component/auth/oauth-button.tsx
@@ -0,0 +1,60 @@
+import type { FC } from 'react';
+import type { ButtonProps } from 'antd';
+
+import { useState } from 'react';
+import { googleOAuthService } from '~service/oauth.service';
+
+import { Button, notification } from 'antd';
+import { GoogleOutlined } from '@ant-design/icons';
+import { alertError } from '~commons/alert-error/alert-error';
+
+import { BroadcastChannelName } from '~constant/broadcast-channel';
+
+export const GoogleOAuthButton: FC<ButtonProps> = props => {
+    const [loading, setLoading] = useState(false);
+    const [api, context] = notification.useNotification();
+
+    async function redirect() {
+        setLoading(true);
+
+        const [url, err] = await googleOAuthService.getUrl();
+
+        if (err) {
+            api.error(alertError(err));
+        } else {
+            globalThis.open(url, '_blank');
+
+            const bc = new BroadcastChannel(BroadcastChannelName.AuthBroadcast);
+
+            bc.onmessage = event => {
+                console.log(event);
+
+                if (event.data === 'success') {
+                    globalThis.location.reload();
+                    bc.close();
+                }
+
+                if (event.data === 'error') bc.close();
+            };
+        }
+
+        setLoading(false);
+    }
+
+    return (
+        <>
+            <Button
+                {...props}
+                color="red"
+                variant="solid"
+                icon={<GoogleOutlined />}
+                onClick={redirect}
+                loading={loading}
+            >
+                Войти с помощью Google
+            </Button>
+
+            {context}
+        </>
+    );
+};
