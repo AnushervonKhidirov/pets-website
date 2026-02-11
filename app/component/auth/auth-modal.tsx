0c12825a786ec76f1d31f2281a40422010a20e01
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-11 09:59:21 +0500

    fetch user data on success login

--- app/component/auth/auth-modal.tsx
+++ app/component/auth/auth-modal.tsx
@@ -1,10 +1,14 @@
 import type { FC, ReactNode, CSSProperties } from 'react';
 import type { ButtonProps } from 'antd';
-import { useState } from 'react';
 
-import { Modal, Button } from 'antd';
+import { useState } from 'react';
+import useUserStore from '~store/user.store';
+import userService from '~service/user.service';
+
+import { Modal, Button, notification } from 'antd';
 import { SignInForm, SignUpForm } from './auth-form';
 import { GoogleOAuthButton } from './oauth-button';
+import { alertError } from '~commons/alert-error/alert-error';
 
 type AuthModalProps = {
     open: boolean;
@@ -16,7 +20,10 @@ type AuthFooterProps = { content: AuthContent; setContent: (state: AuthContent) 
 export type AuthContent = 'sign_in' | 'sign_up';
 
 const AuthModal: FC<AuthModalProps> = ({ open, setOpen, contentType }) => {
+    const { setUser } = useUserStore(state => state);
     const [content, setContent] = useState<AuthContent>(contentType);
+
+    const [api, context] = notification.useNotification();
 
     const title: Record<AuthContent, string> = {
         sign_in: 'Вход',
@@ -27,30 +34,41 @@ const AuthModal: FC<AuthModalProps> = ({ open, setOpen, contentType }) => {
         if (!isOpen) setContent(contentType);
     }
 
-    function closeModal() {
+    async function closeModal() {
+        const [user, err] = await userService.getMe();
+        if (err) {
+            api.error(alertError(err));
+        } else {
+            setUser(user);
+        }
+
         setOpen(false);
     }
 
     return (
-        <Modal
-            afterOpenChange={afterOpenHandler}
-            title={title[content]}
-            open={open}
-            onCancel={closeModal}
-            centered
-            footer={<AuthFooter content={content} setContent={setContent} />}
-            style={{ maxWidth: 400 }}
-        >
-            {content === 'sign_in' ? (
-                <SignInForm onSuccess={closeModal} />
-            ) : (
-                <SignUpForm onSuccess={closeModal} />
-            )}
+        <>
+            <Modal
+                afterOpenChange={afterOpenHandler}
+                title={title[content]}
+                open={open}
+                onCancel={closeModal}
+                centered
+                footer={<AuthFooter content={content} setContent={setContent} />}
+                style={{ maxWidth: 400 }}
+            >
+                {content === 'sign_in' ? (
+                    <SignInForm onSuccess={closeModal} />
+                ) : (
+                    <SignUpForm onSuccess={closeModal} />
+                )}
 
-            <div style={OAuthWrapperStyle}>
-                <GoogleOAuthButton block />
-            </div>
-        </Modal>
+                <div style={OAuthWrapperStyle}>
+                    <GoogleOAuthButton block />
+                </div>
+            </Modal>
+
+            {context}
+        </>
     );
 };
 
