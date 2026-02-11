99b91b41e42eeda7e539345d4569f74ef5d4506b
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-11 11:59:36 +0500

    user type updated

--- app/core/type/user.type.ts
+++ app/core/type/user.type.ts
@@ -1,8 +1,23 @@
 export type User = {
     id: number;
+    authType: AuthType;
     email: string;
-    phone: string;
+    phone: string | null;
     firstName: string;
     lastName: string | null;
     contacts: string | null;
+    address: Address[];
 };
+
+export type Address = {
+    id: number;
+    address: string;
+    latitude: string;
+    longitude: string;
+};
+
+export enum AuthType {
+    Local = 'Local',
+    Google = 'Google',
+    Yandex = 'Yandex',
+}
