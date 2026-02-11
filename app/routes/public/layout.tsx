91ac93fc1130919d8fa0d02d5753b247764abdee
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-11 09:11:29 +0500

    add fetching user

--- app/routes/public/layout.tsx
+++ app/routes/public/layout.tsx
@@ -1,9 +1,25 @@
 import { Outlet } from 'react-router';
+import { useAuth } from '~hook/use-auth';
 
 import Header from '~commons/header/header';
 import Footer from '~commons/footer/footer';
 
+import userService from '~service/user.service';
+import useUserStore from '~/core/store/user.store';
+
 export const Layout = () => {
+    const { update } = useUserStore(state => state);
+
+    async function fetchUserData() {
+        const [user, err] = await userService.getMe();
+        if (err) return;
+        update(user);
+    }
+
+    useAuth(() => {
+        fetchUserData();
+    });
+
     return (
         <>
             <Header />
