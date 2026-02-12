2c56fe1e142242c5d858affb9a66eb0af2235785
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-12 10:41:53 +0500

    isContactItem checker added

--- app/core/constant/contact-links.ts
+++ app/core/constant/contact-links.ts
@@ -1,4 +1,14 @@
-export const ContactLinks: Record<string, string> = {
+export enum ContactName {
+    Telegram = 'Telegram',
+    WhatsApp = 'WhatsApp',
+}
+
+export const ContactLinks: Record<Lowercase<ContactName>, string | null> = {
     telegram: 'https://t.me/',
     whatsapp: 'https://wa.me/',
 };
+
+export function isContactItem(name: string): name is ContactName {
+    const contactNames = Object.values(ContactName) as string[];
+    return contactNames.includes(name);
+}
