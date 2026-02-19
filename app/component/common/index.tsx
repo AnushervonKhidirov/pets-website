dbef5e53fd9c23f1937e7e43127ae828ed350728
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-19 14:12:08 +0500

    pet card removed from common

--- app/component/common/index.tsx
+++ app/component/common/index.tsx
@@ -1,6 +1,5 @@
 import Background from './background';
 import Card from './card';
-import PetCard from './pet-card';
 import Container from './container';
 import Grid from './grid';
 import Logo from './logo';
@@ -10,5 +9,5 @@ import GoogleMap from './google-map';
 import Footer from './footer';
 import Header from './header';
 
-export { Background, Card, PetCard, Container, Grid, Logo, UserAvatar, GoogleMap, Footer, Header };
+export { Background, Card, Container, Grid, Logo, UserAvatar, GoogleMap, Footer, Header };
 export { PhoneLink, Contacts } from './user-contact';
