7660c482cf69a5fbd7711c5d83915aaefb5aeb65
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-19 19:00:06 +0500

    actions added

--- app/component/pet/pet-info-card/index.tsx
+++ app/component/pet/pet-info-card/index.tsx
@@ -1,10 +1,12 @@
 import type { FC, JSX } from 'react';
+import type { DropdownProps, MenuProps } from 'antd';
 import type { Pet, PetWithUser } from '~type/pet.type';
 import type { WithAdditionalProps } from '~type/common.type';
 
-import { Tag, Typography } from 'antd';
+import { Dropdown, Tag, Typography } from 'antd';
 import { red } from '@ant-design/colors';
-import { cyan, gray, light } from '~/config/ant.config';
+import { Background, Card, UserAvatar, PhoneLink, Contacts } from '~component/common';
+import { MoreOutlined } from '@ant-design/icons';
 import {
     PawIcon,
     TickIcon,
@@ -14,8 +16,8 @@ import {
     CalendarIcon,
     PhoneIcon,
 } from '~icons';
-import { Background, Card, UserAvatar, PhoneLink, Contacts } from '~component/common';
 
+import { cyan, gray, light } from '~/config/ant.config';
 import { sex } from '~constant/pet';
 import placeholder from 'src/images/pet-placeholder.png';
 
@@ -24,10 +26,14 @@ import classes from './pet-info-card.module.css';
 
 const { Title, Text } = Typography;
 
-type InfoItem = { icon: JSX.Element; headline: string; value: string | number; isDanger?: boolean };
-type PetInfoCardProps = WithAdditionalProps<{ pet: Pet | PetWithUser }>;
+type PetInfoCardProps = WithAdditionalProps<{
+    pet: Pet | PetWithUser;
+    actions?: MenuProps['items'];
+}>;
 
-const PetInfoCard: FC<PetInfoCardProps> = ({ pet, children }) => {
+type InfoItem = { icon: JSX.Element; headline: string; value: string | number; isDanger?: boolean };
+
+const PetInfoCard: FC<PetInfoCardProps> = ({ pet, children, actions }) => {
     const infoItem: InfoItem[] = [
         {
             icon: <PawIcon />,
@@ -62,7 +68,7 @@ const PetInfoCard: FC<PetInfoCardProps> = ({ pet, children }) => {
             style={{ borderRadius: '1.25rem' }}
             classNames={{ root: classes.card }}
             innerClassName={classes.card_body}
-            cover={<CardImage pet={pet} />}
+            cover={<CardHeader pet={pet} actions={actions} />}
         >
             <div className={classes.about_pet}>
                 <Title level={3} style={{ marginBottom: '0.5em' }}>
@@ -87,6 +93,82 @@ const PetInfoCard: FC<PetInfoCardProps> = ({ pet, children }) => {
             {'user' in pet && <OwnerCard user={pet.user} />}
 
             {children}
+        </Card>
+    );
+};
+
+const CardHeader: FC<{ pet: Pet; actions?: MenuProps['items'] }> = ({ pet, actions }) => {
+    const sharedProps: DropdownProps = {
+        menu: { items: actions },
+        placement: 'bottomRight',
+        trigger: ['click'],
+        styles: { item: { padding: 0 } },
+    };
+
+    return (
+        <Background
+            className={classes.header}
+            color={gray[2]}
+            style={{ aspectRatio: '1/0.5', display: 'grid' }}
+        >
+            <img
+                src={pet.image ?? placeholder}
+                alt={pet.name}
+                className={classNames(classes.image, { [classes.placeholder]: !pet.image })}
+            />
+
+            <div className={classes.header_overlay}>
+                <div className={classes.name}>{pet.name}</div>
+                {pet.breed?.ru && <div className={classes.breed}>{pet.breed.ru}</div>}
+                <StatusTag lost={pet.lost} />
+
+                {actions && (
+                    <Dropdown {...sharedProps}>
+                        <Background
+                            style={{ width: 'auto' }}
+                            className={classes.more_btn}
+                        >
+                            <MoreOutlined />
+                        </Background>
+                    </Dropdown>
+                )}
+            </div>
+        </Background>
+    );
+};
+
+const StatusTag: FC<{ lost: boolean }> = ({ lost }) => {
+    return lost ? (
+        <Tag
+            color="error"
+            variant="solid"
+            icon={<WarningIcon />}
+            className={classNames(classes.tag, { [classes.lost]: lost })}
+        >
+            Потерян
+        </Tag>
+    ) : (
+        <Tag color="success" variant="outlined" icon={<TickIcon />} className={classes.tag}>
+            Дома
+        </Tag>
+    );
+};
+
+const InfoCard: FC<InfoItem> = ({ icon, headline, value, isDanger }) => {
+    const cardColor = isDanger ? red[0] : light;
+    const headlineColor = isDanger ? red[5] : cyan[5];
+
+    return (
+        <Card
+            color={cardColor}
+            innerClassName={classes.info_card}
+            variant={isDanger ? 'outlined' : 'borderless'}
+            style={{ borderColor: red[2] }}
+        >
+            <div className={classes.info_card_headline} style={{ color: headlineColor }}>
+                {icon} <span>{headline}</span>
+            </div>
+            <div className={classes.info_card_value}>{value}</div>
         </Card>
     );
 };
@@ -120,64 +202,6 @@ const OwnerCard: FC<{ user: PetWithUser['user'] }> = ({ user }) => {
                 </div>
             </Card>
         </div>
-    );
-};
-
-const InfoCard: FC<InfoItem> = ({ icon, headline, value, isDanger }) => {
-    const cardColor = isDanger ? red[0] : light;
-    const headlineColor = isDanger ? red[5] : cyan[5];
-
-    return (
-        <Card
-            color={cardColor}
-            innerClassName={classes.info_card}
-            variant={isDanger ? 'outlined' : 'borderless'}
-            style={{ borderColor: red[2] }}
-        >
-            <div className={classes.info_card_headline} style={{ color: headlineColor }}>
-                {icon} <span>{headline}</span>
-            </div>
-            <div className={classes.info_card_value}>{value}</div>
-        </Card>
-    );
-};
-
-const CardImage: FC<{ pet: Pet }> = ({ pet }) => {
-    return (
-        <Background
-            className={classes.header}
-            color={gray[2]}
-            style={{ aspectRatio: '1/0.5', display: 'grid' }}
-        >
-            <img
-                src={pet.image ?? placeholder}
-                alt={pet.name}
-                className={classNames(classes.image, { [classes.placeholder]: !pet.image })}
-            />
-
-            <div className={classes.header_overlay}>
-                <div className={classes.name}>{pet.name}</div>
-                {pet.breed?.ru && <div className={classes.breed}>{pet.breed.ru}</div>}
-                <StatusTag lost={pet.lost} />
-            </div>
-        </Background>
-    );
-};
-
-const StatusTag: FC<{ lost: boolean }> = ({ lost }) => {
-    return lost ? (
-        <Tag
-            color="error"
-            variant="solid"
-            icon={<WarningIcon />}
-            className={classNames(classes.tag, { [classes.lost]: lost })}
-        >
-            Потерян
-        </Tag>
-    ) : (
-        <Tag color="success" variant="outlined" icon={<TickIcon />} className={classes.tag}>
-            Дома
-        </Tag>
     );
 };
 
