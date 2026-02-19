0cfb79162e89c18e935fd18fc148725cdc96813c
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-19 07:52:16 +0500

    new methods added

--- app/core/service/pet.service.ts
+++ app/core/service/pet.service.ts
@@ -1,5 +1,5 @@
 import type { ReturnWithErrPromise } from '~type/common.type';
-import type { Pet, PetType, Breed, CratePetDto } from '~type/pet.type';
+import type { Pet, PetType, Breed, PetDto } from '~type/pet.type';
 
 import dayjs from 'dayjs';
 import { apiClient, apiClientAuth } from '~api/api-client';
@@ -61,14 +61,33 @@ class PetService {
         }
     }
 
-    async create({ birthday, ...data }: CratePetDto): ReturnWithErrPromise<Pet> {
-        const convertedData: Omit<CratePetDto, 'birthday'> & { birthday: string | null } = {
-            ...data,
-            birthday: birthday?.startOf('date').toString() ?? null,
-        };
+    async create(data: PetDto): ReturnWithErrPromise<Pet> {
+        const convertedData = this.prepareDataToPush(data);
 
         try {
             const pet = await apiClientAuth.post<PetResponse>('/pet', convertedData);
+            if (isHttpException(pet.data)) throw pet.data;
+            return [this.convertData(pet.data), null];
+        } catch (err) {
+            return errorHandler(err);
+        }
+    }
+
+    async update(id: number, data: PetDto): ReturnWithErrPromise<Pet> {
+        const convertedData = this.prepareDataToPush(data);
+
+        try {
+            const pet = await apiClientAuth.patch<PetResponse>(`/pet/${id}`, convertedData);
+            if (isHttpException(pet.data)) throw pet.data;
+            return [this.convertData(pet.data), null];
+        } catch (err) {
+            return errorHandler(err);
+        }
+    }
+
+    async delete(id: number): ReturnWithErrPromise<Pet> {
+        try {
+            const pet = await apiClientAuth.delete<PetResponse>(`/pet/${id}`);
             if (isHttpException(pet.data)) throw pet.data;
             return [this.convertData(pet.data), null];
         } catch (err) {
@@ -81,11 +100,20 @@ class PetService {
             const formData = new FormData();
             formData.append('image', file);
 
-            const pet = await apiClientAuth.post('/pet/image', formData, {
-                params: { petId },
+            const pet = await apiClientAuth.post(`/pet/image/${petId}`, formData, {
                 headers: { 'Content-Type': 'multipart/form-data' },
             });
 
+            if (isHttpException(pet.data)) throw pet.data;
+            return [pet.data, null];
+        } catch (err) {
+            return errorHandler(err);
+        }
+    }
+
+    async deleteImage(petId: number): ReturnWithErrPromise {
+        try {
+            const pet = await apiClientAuth.delete(`/pet/image/${petId}`);
             if (isHttpException(pet.data)) throw pet.data;
             return [pet.data, null];
         } catch (err) {
@@ -97,6 +125,22 @@ class PetService {
         const birthday = dayjs(pet.birthday).isValid() ? dayjs(pet.birthday) : null;
         const image = pet.image ? `${serverUrl}/pet/image/${pet.image}` : null;
         return { ...pet, birthday, image };
+    }
+
+    private prepareDataToPush({ birthday, ...data }: PetDto) {
+        const convertedData: Record<keyof PetDto, string | number | null> = {
+            ...data,
+            birthday: birthday?.startOf('date').toString() ?? null,
+        };
+
+        if ('image' in convertedData) delete convertedData.image;
+
+        for (const key in convertedData) {
+            const k = key as keyof typeof convertedData;
+            if (!convertedData[k]) convertedData[k] = null;
+        }
+
+        return convertedData;
     }
 }
 
