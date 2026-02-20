f10ee915c5511a632bb0c5fb2761680a079814ad
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-20 18:51:15 +0500

    tanstack query added

--- app/root.tsx
+++ app/root.tsx
@@ -3,11 +3,15 @@ import type { Route } from './+types/root';
 
 import { isRouteErrorResponse, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
 import { ConfigProvider } from 'antd';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { themeConfig } from './config/ant.config';
 import './config/dayjs.config';
 
 import './styles/fonts.css';
 import './styles/root.css';
+
+const queryClient = new QueryClient({ defaultOptions: { queries: { retry: 0 } } });
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
     return (
@@ -18,7 +22,9 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
                 <Meta />
             </head>
             <body className="custom-ant">
-                <ConfigProvider theme={themeConfig}>{children}</ConfigProvider>
+                <ConfigProvider theme={themeConfig}>
+                    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+                </ConfigProvider>
                 <ScrollRestoration />
                 <Scripts />
             </body>
