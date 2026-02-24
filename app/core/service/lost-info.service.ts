3318b2539dc6608fa5bf60047f4fd26966150868
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-24 20:35:19 +0500

    lost info service added

---
+++ app/core/service/lost-info.service.ts
@@ -0,0 +1,65 @@
+import type { ReturnWithErrPromise } from '~type/common.type';
+import type { LostInfo, LostInfoDto } from '~type/lost-info.type';
+
+import dayjs from 'dayjs';
+import { apiClientAuth } from '~api/api-client';
+import { errorHandler, isHttpException } from '~helper/error-handler';
+
+type LostInfoResponse = Omit<LostInfo, 'lostAt'> & { lostAt: string | null };
+
+class LostInfoService {
+    async create(petId: number, data: LostInfoDto): ReturnWithErrPromise<LostInfo> {
+        const convertedData = this.prepareDataToPush(data);
+
+        try {
+            const pet = await apiClientAuth.post<LostInfoResponse>(
+                `lost-info/${petId}`,
+                convertedData,
+            );
+            if (isHttpException(pet.data)) throw pet.data;
+            return [this.convertData(pet.data), null];
+        } catch (err) {
+            return errorHandler(err);
+        }
+    }
+
+    async update(petId: number, data: LostInfoDto): ReturnWithErrPromise<LostInfo> {
+        const convertedData = this.prepareDataToPush(data);
+
+        try {
+            const pet = await apiClientAuth.patch<LostInfoResponse>(
+                `lost-info/${petId}`,
+                convertedData,
+            );
+            if (isHttpException(pet.data)) throw pet.data;
+            return [this.convertData(pet.data), null];
+        } catch (err) {
+            return errorHandler(err);
+        }
+    }
+
+    async delete(petId: number): ReturnWithErrPromise {
+        try {
+            const pet = await apiClientAuth.delete<LostInfoResponse>(`lost-info/${petId}`);
+            if (isHttpException(pet.data)) throw pet.data;
+            return [null, null];
+        } catch (err) {
+            return errorHandler(err);
+        }
+    }
+
+    private convertData(lostInfo: LostInfoResponse): LostInfo {
+        const lostAt = dayjs(lostInfo.lostAt);
+        return { ...lostInfo, lostAt };
+    }
+
+    private prepareDataToPush({ lostAt, ...data }: LostInfoDto) {
+        return {
+            details: data.details ?? null,
+            address: data.address ?? null,
+            lostAt: lostAt.startOf('date').toString() ?? null,
+        };
+    }
+}
+
+export default new LostInfoService();
