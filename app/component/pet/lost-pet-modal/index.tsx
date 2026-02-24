b68016b56bc835a922b3110d9cd9a16cb04027e4
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-24 20:35:49 +0500

    lost pet modal added

---
+++ app/component/pet/lost-pet-modal/index.tsx
@@ -0,0 +1,122 @@
+import type { FC } from 'react';
+import type { LostInfo, LostInfoDto } from '~type/lost-info.type';
+
+import { useState } from 'react';
+import useMyPetsStore from '~store/my-pets.store';
+import lostInfoService from '~service/lost-info.service';
+
+import { Modal, Form, Input, Button, DatePicker, notification } from 'antd';
+import dayjs from 'dayjs';
+import { alertError } from '~helper/alert-error';
+
+import classes from './lost-pet-modal.module.css';
+
+type EditPersonalInfoModalProps = {
+    petId: number;
+    lostInfo?: LostInfo | null;
+    open: boolean;
+    setOpen: (state: boolean) => void;
+};
+
+const LostInfoModal: FC<EditPersonalInfoModalProps> = ({ petId, lostInfo, open, setOpen }) => {
+    const [form] = Form.useForm();
+    const { pets, updatePet } = useMyPetsStore(state => state);
+
+    const [loading, setLoading] = useState(false);
+    const [loadingRemove, setLoadingRemove] = useState(false);
+
+    const [api, context] = notification.useNotification();
+
+    async function submit(data: LostInfoDto) {
+        setLoading(true);
+
+        const [lostInfo, err] = await lostInfoHandler(petId, data);
+
+        if (err) {
+            api.error(alertError(err));
+        } else {
+            const pet = pets.find(pet => pet.id === petId);
+            if (pet) updatePet({ ...pet, lostInfo: lostInfo });
+        }
+
+        setLoading(false);
+
+        setTimeout(() => setOpen(false), 500);
+    }
+
+    async function remove() {
+        setLoadingRemove(true);
+
+        const [, err] = await lostInfoService.delete(petId);
+
+        if (err) {
+            api.error(alertError(err));
+        } else {
+            const pet = pets.find(pet => pet.id === petId);
+            if (pet) updatePet({ ...pet, lostInfo: null });
+        }
+
+        setLoadingRemove(false);
+        setTimeout(() => setOpen(false), 500);
+    }
+
+    async function lostInfoHandler(petId: number, data: LostInfoDto) {
+        if (lostInfo) {
+            return await lostInfoService.update(petId, data);
+        } else {
+            return await lostInfoService.create(petId, data);
+        }
+    }
+
+    return (
+        <>
+            <Modal
+                title="Заявление о пропаже"
+                open={open}
+                onCancel={() => setOpen(false)}
+                centered
+                footer={null}
+                destroyOnHidden
+            >
+                <Form onFinish={submit} form={form} preserve>
+                    <Form.Item name="address" initialValue={lostInfo?.address ?? null}>
+                        <Input placeholder="Адрес" />
+                    </Form.Item>
+
+                    <Form.Item name="details" initialValue={lostInfo?.details ?? null}>
+                        <Input placeholder="Подробности" />
+                    </Form.Item>
+
+                    <Form.Item
+                        name="lostAt"
+                        initialValue={lostInfo?.lostAt ?? dayjs()}
+                        rules={[{ required: true, message: 'Укажите дату пропажи' }]}
+                    >
+                        <DatePicker
+                            format={{ format: 'DD/MM/YYYY' }}
+                            placeholder="День пропажи (день/месяц/год)"
+                            allowClear
+                            style={{ width: '100%' }}
+                        />
+                    </Form.Item>
+
+                    <div className={classes.buttons}>
+                        {lostInfo && (
+                            <Button danger type="primary" loading={loadingRemove} onClick={remove}>
+                                Удалить объявление
+                            </Button>
+                        )}
+
+                        <Button color="cyan" variant="solid" htmlType="submit" loading={loading}>
+                            {lostInfo ? 'Сохранить объявление' : 'Создать объявление'}
+                        </Button>
+                    </div>
+                </Form>
+            </Modal>
+
+            {context}
+        </>
+    );
+};
+
+export default LostInfoModal;
