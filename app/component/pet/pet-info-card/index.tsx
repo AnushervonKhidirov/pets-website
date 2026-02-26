0c2b7e8e57be18e9eac84b5e090bc44e7cd7a0d7
AnushervonKhidirov <ankhid98@gmail.com>
2026-02-26 13:59:56 +0500

    Pet info card updated

--- app/component/pet/pet-info-card/index.tsx
+++ app/component/pet/pet-info-card/index.tsx
@@ -1,24 +1,19 @@
-import type { FC, JSX } from 'react';
+import type { CSSProperties, FC } from 'react';
 import type { DropdownProps, MenuProps } from 'antd';
-import type { Pet, PetWithUser } from '~type/pet.type';
 import type { WithAdditionalProps } from '~type/common.type';
+import type { Pet, PetWithUser } from '~type/pet.type';
+import type { LostInfo } from '~type/lost-info.type';
 
-import { Dropdown, Tag, Typography } from 'antd';
+import { Dropdown, Tag, Typography, Descriptions } from 'antd';
 import { red } from '@ant-design/colors';
-import { Background, Card, UserAvatar, PhoneLink, Contacts } from '~component/common';
+import { Background, Card, UserAvatar, PhoneLink, Contacts, QRCode } from '~component/common';
 import { MoreOutlined } from '@ant-design/icons';
-import {
-    PawIcon,
-    TickIcon,
-    WarningIcon,
-    GenderIcon,
-    HashIcon,
-    CalendarIcon,
-    PhoneIcon,
-} from '~icons';
+import { TickIcon, WarningIcon, PhoneIcon } from '~icons';
+import { LostPetButton } from '../pet-action-buttons';
 
-import { cyan, gray, light } from '~/config/ant.config';
+import { cyan, gray } from '~/config/ant.config';
 import { sex } from '~constant/pet';
+import { Route } from '~constant/route';
 import placeholder from 'src/images/pet-placeholder.png';
 
 import classNames from 'classnames';
@@ -28,71 +23,90 @@ const { Title, Text } = Typography;
 
 type PetInfoCardProps = WithAdditionalProps<{
     pet: Pet | PetWithUser;
+    hoverable?: boolean;
+    hideBody?: boolean;
+    showQR?: boolean;
+    showOwner?: boolean;
+    showLostBtn?: boolean;
     actions?: MenuProps['items'];
 }>;
 
-type InfoItem = { icon: JSX.Element; headline: string; value: string | number; isDanger?: boolean };
+const PetInfoCard: FC<PetInfoCardProps> = ({
+    pet,
+    hoverable,
+    hideBody,
+    showQR,
+    showOwner,
+    showLostBtn,
+    actions,
+}) => {
+    const petProfilePage = `${Route.PetInfo}/${pet.id}`;
 
-const PetInfoCard: FC<PetInfoCardProps> = ({ pet, children, actions }) => {
-    const infoItem: InfoItem[] = [
+    const infoItems = [
         {
-            icon: <PawIcon />,
-            headline: 'Вид',
-            value: pet.petType.ru,
+            key: pet.id + 'petType',
+            label: 'Вид',
+            children: pet.petType.ru,
         },
         {
-            icon: <CalendarIcon />,
-            headline: 'Возраст',
-            value: pet.birthday?.isValid() ? pet.birthday.toNow(true) : '',
+            key: pet.id + 'birthday',
+            label: 'Возраст',
+            children: pet.birthday?.isValid() ? pet.birthday.toNow(true) : null,
         },
         {
-            icon: <GenderIcon />,
-            headline: 'Пол',
-            value: pet.sex ? sex[pet.sex].ru : '',
+            key: pet.id + 'sex',
+            label: 'Пол',
+            children: pet.sex ? sex[pet.sex].ru : null,
         },
         {
-            icon: <HashIcon />,
-            headline: 'Чип',
-            value: pet.microchipId ?? '',
+            key: pet.id + 'microchipId',
+            label: 'Чип',
+            children: pet.microchipId ?? null,
         },
-        {
-            icon: <WarningIcon />,
-            headline: 'Дата пропажи',
-            value: pet.lostInfo?.lostAt.startOf('day').format('DD MMMM YYYY') ?? '',
-            isDanger: true,
-        },
-    ].filter(item => Boolean(item.value));
+    ].filter(item => item.children);
 
     return (
         <Card
+            hoverable={hoverable}
             style={{ borderRadius: '1.25rem' }}
             classNames={{ root: classes.card }}
             innerClassName={classes.card_body}
             cover={<CardHeader pet={pet} actions={actions} />}
         >
-            <div className={classes.about_pet}>
-                <Title level={3} style={{ marginBottom: '0.5em' }}>
-                    О питомце
-                </Title>
+            {!hideBody && (
+                <>
+                    <div className={classes.main_info}>
+                        <div>
+                            <Title level={3} style={{ marginBottom: '0.25em' }}>
+                                О питомце
+                            </Title>
 
-                {pet.about && <Text>{pet.about}</Text>}
+                            {pet.about && <Text>{pet.about}</Text>}
+                        </div>
 
-                <div className={classes.info_card_wrapper}>
-                    {infoItem.map(({ icon, headline, value, isDanger = undefined }) => (
-                        <InfoCard
-                            key={value}
-                            icon={icon}
-                            headline={headline}
-                            value={value}
-                            isDanger={isDanger}
+                        <Descriptions
+                            column={{ md: 2, lg: 2, xl: 2 }}
+                            styles={{ label: { color: cyan[5], fontWeight: 500 } }}
+                            items={infoItems}
                         />
-                    ))}
-                </div>
-            </div>
 
-            {'user' in pet && <OwnerCard user={pet.user} />}
+                        <div>{pet.lostInfo && <LostInfoCard lostInfo={pet.lostInfo} />}</div>
+                    </div>
 
-            {children ? <div>{children}</div> : null}
+                    <div className={classes.additional_info}>
+                        {showQR && (
+                            <QRCode
+                                name={pet.name}
+                                value={globalThis.location.origin + petProfilePage}
+                            />
+                        )}
+
+                        {'user' in pet && showOwner && <OwnerCard user={pet.user} />}
+
+                        {showLostBtn && <LostPetButton pet={pet} block size="large" />}
+                    </div>
+                </>
+            )}
         </Card>
     );
 };
@@ -151,21 +165,45 @@ const StatusTag: FC<{ lost: boolean }> = ({ lost }) => {
     );
 };
 
-const InfoCard: FC<InfoItem> = ({ icon, headline, value, isDanger }) => {
-    const cardColor = isDanger ? red[0] : light;
-    const headlineColor = isDanger ? red[5] : cyan[5];
+const LostInfoCard: FC<{ lostInfo: LostInfo; style?: CSSProperties }> = ({ lostInfo, style }) => {
+    const lostInfoItems = [
+        {
+            key: lostInfo.id + lostInfo.petId + 'date',
+            label: 'Дата',
+            children: lostInfo.lostAt.startOf('day').format('DD MMMM YYYY'),
+        },
+        {
+            key: lostInfo.id + lostInfo.petId + 'place',
+            label: 'Место',
+            children: lostInfo.address,
+        },
+        {
+            key: lostInfo.id + lostInfo.petId + 'details',
+            label: 'Обстоятельства',
+            children: lostInfo.details,
+        },
+    ].filter(item => item.children);
 
     return (
         <Card
-            color={cardColor}
-            innerClassName={classNames(classes.info_card, { [classes.danger]: isDanger })}
-            variant={isDanger ? 'outlined' : 'borderless'}
-            style={{ borderColor: red[2] }}
+            color={red[0]}
+            innerClassName={classNames(classes.lost_info_card, classes.danger)}
+            variant="outlined"
+            style={{ borderColor: red[2], ...style }}
         >
-            <div className={classes.info_card_headline} style={{ color: headlineColor }}>
-                {icon} <span>{headline}</span>
+            <div
+                className={classNames(classes.lost_info_card_headline, classes.info_title)}
+                style={{ color: red[5] }}
+            >
+                <WarningIcon /> <span>Информация о пропаже</span>
             </div>
-            <div className={classes.info_card_value}>{value}</div>
+
+            <Descriptions
+                column={1}
+                layout="vertical"
+                styles={{ label: { color: '#000', fontWeight: 700 } }}
+                items={lostInfoItems}
+            />
         </Card>
     );
 };
@@ -175,7 +213,7 @@ const OwnerCard: FC<{ user: PetWithUser['user'] }> = ({ user }) => {
         <div className={classes.about_owner}>
             <Card color="#f9fafb" variant="outlined">
                 <Title level={4} style={{ marginBottom: '1rem' }}>
-                    Раб питомца
+                    Владелец
                 </Title>
 
                 <div className={classes.user_info}>
