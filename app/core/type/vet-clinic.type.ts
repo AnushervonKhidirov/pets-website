ff77058e145755d773300cae33dce52ec2178181
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-03 11:34:20 +0500

    vet clinic type added

---
+++ app/core/type/vet-clinic.type.ts
@@ -0,0 +1,10 @@
+export type VetClinic = {
+    id: number;
+    name: string;
+    address: string;
+    latitude: number;
+    longitude: number;
+    contacts: string[];
+    about: string | null;
+    image: string | null;
+};
