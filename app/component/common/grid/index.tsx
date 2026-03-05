4e393c78bac90df177d12726f81daf421b74d1b9
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-05 22:28:05 +0500

    style added to grid props

--- app/component/common/grid/index.tsx
+++ app/component/common/grid/index.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import type { FC, CSSProperties } from 'react';
 import type { WithAdditionalProps } from '~type/common.type';
 
 import classNames from 'classnames';
@@ -6,10 +6,15 @@ import classes from './grid.module.css';
 
 type GridProps = WithAdditionalProps<{
     size?: 'small' | 'medium' | 'large' | 'full-line';
+    style?: CSSProperties;
 }>;
 
-const Grid: FC<GridProps> = ({ size = 'medium', className, children }) => {
-    return <div className={classNames(classes.grid, classes[size], className)}>{children}</div>;
+const Grid: FC<GridProps> = ({ size = 'medium', className, children, style }) => {
+    return (
+        <div className={classNames(classes.grid, classes[size], className)} style={style}>
+            {children}
+        </div>
+    );
 };
 
 export default Grid;
