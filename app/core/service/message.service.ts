cc72908fb7e2f4185b0328a9b1d7ca60abfe330c
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-05 11:08:00 +0500

    message service added

---
+++ app/core/service/message.service.ts
@@ -0,0 +1,20 @@
+import type { ReturnWithErrPromise, MessageDataDto } from '~type/common.type';
+
+import { apiClient } from '~api/api-client';
+import { errorHandler, HttpException, isHttpException } from '~helper/error-handler';
+
+class MessageService {
+    private readonly endpoint = '/message/';
+
+    async send(data: MessageDataDto): ReturnWithErrPromise {
+        try {
+            const response = await apiClient.post(this.endpoint, data);
+            if (isHttpException(response.data)) throw new HttpException(response.data);
+            return [null, null];
+        } catch (err) {
+            return errorHandler(err);
+        }
+    }
+}
+
+export default new MessageService();
