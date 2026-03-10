fea7755dbaea35ba98fc03252a3adfd333fec2ec
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-10 13:51:41 +0500

    pet setting page added

---
+++ app/routes/private/pet-setting/index.tsx
@@ -0,0 +1,43 @@
+import type { Route as TRoute } from './+types/index';
+
+import { useParams } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
+import petService from '~service/pet.service';
+
+import { Typography } from 'antd';
+import { Container, Loader, ErrorInfo } from '~component/common';
+
+import PetForm from '~component/pet/pet-form';
+
+export function meta() {
+    return [{ title: 'Редактирование питомца' }];
+}
+
+const fetchPet = petService.getMyOne.bind(petService);
+
+const { Title } = Typography;
+
+const MyPetSettingPage = () => {
+    const params = useParams<TRoute.LoaderArgs['params']>();
+    const petId = Number.parseInt(params.petId ?? '0');
+
+    const { isPending, isError, isSuccess, error, data } = useQuery({
+        queryKey: ['fetch_my_pet', `fetch_my_pet_${petId}`],
+        queryFn: fetchPet.bind(null, petId),
+    });
+
+    if (isPending) return <Loader />;
+    if (isError) return <ErrorInfo error={error} />;
+
+    return (
+        isSuccess && (
+            <Container section style={{ minHeight: '100%' }}>
+                <Title level={3}>Редактирование питомца</Title>
+
+                <PetForm pet={data} />
+            </Container>
+        )
+    );
+};
+
+export default MyPetSettingPage;
