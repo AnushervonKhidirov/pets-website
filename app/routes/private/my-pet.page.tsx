e0f3a76b7cf0c2ccaf72e93df664afce36fcbabe
AnushervonKhidirov <ankhid98@gmail.com>
2026-03-10 11:34:45 +0500

    my pet page refactored

--- app/routes/private/my-pet.page.tsx
+++ app/routes/private/my-pet.page.tsx
@@ -4,19 +4,24 @@ import type { MenuProps } from 'antd';
 import type { Pet } from '~type/pet.type';
 
 import { useState } from 'react';
-import { useParams, useNavigate } from 'react-router';
-import { useQuery } from '@tanstack/react-query';
+import { Link, useParams, useNavigate } from 'react-router';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import petService from '~service/pet.service';
 
+import { Button, Modal, notification } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import { Container, Loader, ErrorInfo } from '~component/common';
 import PetInfoCard from '~component/pet/pet-info-card';
-import { DeletePetButton, EditPetButton, LostPetButton } from '~component/pet/pet-action-buttons';
+import LostInfoModal from '~component/pet/lost-pet-modal';
 
 import { Route } from '~constant/route';
+import { join } from '~helper/path.helper';
 
 export function meta() {
     return [{ title: 'Мой питомец' }];
 }
+
+const deletePet = petService.delete.bind(petService);
 
 const MyPet: FC = () => {
     const params = Number.parseInt(useParams<TRoute.LoaderArgs['params']>().petId!);
@@ -48,41 +53,76 @@ const PetCard: FC<{ pet: Pet }> = ({ pet }) => {
     const navigation = useNavigate();
     const [currPet, setCurrPet] = useState(pet);
 
-    function deletePetHandler() {
-        navigation(Route.MyPets);
-    }
+    const [api, context] = notification.useNotification();
+
+    const [open, setOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
 
     const actions: MenuProps['items'] = [
         {
             key: 'edit',
-            label: (
-                <EditPetButton
-                    block
-                    pet={pet}
-                    color="default"
-                    variant="text"
-                    style={{ justifyContent: 'start' }}
-                    onSuccess={setCurrPet}
-                />
-            ),
+            icon: <EditOutlined />,
+            label: <Link to={join(Route.MyPetSetting, pet.id)}>Редактировать</Link>,
         },
         {
             key: 'delete',
-            label: (
-                <DeletePetButton
-                    block
-                    pet={pet}
-                    type="primary"
-                    style={{ justifyContent: 'start' }}
-                    onSuccess={deletePetHandler}
-                />
-            ),
+            danger: true,
+            icon: <DeleteOutlined />,
+            onClick: () => setOpen(true),
+            label: 'Удалить',
         },
     ];
 
+    const { mutate, isPending } = useMutation({
+        mutationKey: ['delete_pet'],
+        mutationFn: deletePet,
+        onSuccess: pet => {
+            navigation(Route.MyPets);
+        },
+        onError: error => {
+            api.error({ description: error.message });
+        },
+        onSettled: () => {
+            setOpen(false);
+        },
+    });
+
     return (
         <PetInfoCard pet={currPet} showQR actions={actions}>
-            <LostPetButton pet={currPet} block size="large" onSuccess={setCurrPet} />
+            <Button
+                block
+                size="large"
+                danger={!pet.lostInfo}
+                color="cyan"
+                type="primary"
+                variant="solid"
+                onClick={() => setModalOpen(true)}
+            >
+                Объявление о пропаже
+            </Button>
+
+            <LostInfoModal
+                pet={pet}
+                lostInfo={pet.lostInfo}
+                open={modalOpen}
+                setOpen={setModalOpen}
+                onSuccess={setCurrPet}
+            />
+
+            <Modal
+                open={open}
+                title="Удалить питомца?"
+                okText="Удалить"
+                okButtonProps={{ loading: isPending, type: 'primary', danger: true }}
+                cancelText="Отмена"
+                onOk={() => mutate(pet.id)}
+                onCancel={() => setOpen(false)}
+                mask={false}
+                destroyOnHidden
+            >
+                Питомец <b>{pet.name}</b> будет удален
+            </Modal>
+            {context}
         </PetInfoCard>
     );
 };
